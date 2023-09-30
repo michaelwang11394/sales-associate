aaa87451b4ef7bb71b782de4f97745392992acc6
Michael Wang <michaelwang11394@gmail.com>
2023-09-30 14:22:16 -0700

    Add supabase to read and record events

--- extensions/web-pixel/src/index.ts
+++ extensions/web-pixel/src/index.ts
@@ -1,35 +1,30 @@
 import { register } from "@shopify/web-pixels-extension";
+import supabase from "extensions/web-pixel/helpers/supabase";
 
 register(async ({ analytics, browser, settings }) => {
-  // const uid = await browser.cookie.get("your_visitor_cookie");
-
   // subscribe to events
   analytics.subscribe("all_events", async (event) => {
     console.log("web pixel event:", event);
+    const { clientId, context, id, name, timestamp } = event;
 
-    // The Server is http://localhost:51246
-    // const pixelEndpoint =
-    //   "https://ill-handmade-crm-opportunity.trycloudflare.com/ai";
+    try {
+      const { data, error } = await supabase.from("events").insert([
+        {
+          id,
+          timestamp,
+          clientId,
+          context: JSON.stringify(context), // convert context object to JSON string
+          name,
+        },
+      ]);
 
-    // Send event to /ai route in Remix application
-
-    // try {
-    //   const response = await fetch("/ai", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ event }),
-    //     keepalive: true,
-    //   });
-    //   console.log("web pixel event sent", response);
-    // } catch (error) {
-    //   console.error("Error during fetch:", error);
-    // }
-    // const iSuccessful = await browser.sendBeacon(
-    //   pixelEndpoint,
-    //   JSON.stringify({ ...event })
-    // );
-    // if (!isSusccessful) throw new Error("Unable to send via Beacon API");
+      if (error) {
+        console.error("Error during insert:", error);
+      } else {
+        console.log("web pixel event sent", data);
+      }
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
   });
 });
