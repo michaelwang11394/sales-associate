d7224801b71b73553f54e2e0cbfdcf25733afa36
Michael Wang <michaelwang11394@gmail.com>
2023-10-01 19:45:49 -0700

    Integrate-react-app-with-shopify-and-extensions

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: "tw-",
+};
