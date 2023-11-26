0d149fc6eb6a2bf0c622cfd73c74a8e6f98a95c4
oniken <kentaonex@gmail.com>
2023-11-26 14:56:52 -0500

    UI with scrollable suggestions and thread

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,24 +1,19 @@
-import { useEffect, useState, useRef } from "react";
-// @ts-ignore
+import React, { useEffect, useState, useRef } from "react";
 import { subscribeToEvents } from "./supabase.js";
 import { handleNewCustomerEvent } from "./ai.js";
 
-// @ts-ignore
 export default function App({ home }) {
   const [userInput, setUserInput] = useState("");
   const [showSearchBar, setShowSearchBar] = useState(false);
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  const [chatThread, setChatThread] = useState([]);
   const iconRef = useRef(null);
 
   useEffect(() => {
     if (iconRef.current) {
-      // @ts-ignore
       const iconWidth = iconRef.current.clientWidth;
-      // @ts-ignore
       const iconHeight = iconRef.current.clientHeight;
-      // @ts-ignore
       const iconOffsetTop = iconRef.current.offsetTop;
-      // @ts-ignore
       const iconOffsetLeft = iconRef.current.offsetLeft;
 
       console.log("Icon Width:", iconWidth);
@@ -30,51 +25,51 @@ export default function App({ home }) {
     }
   }, []);
 
-  useEffect(() => {
-    // Show dropdown as soon as there is text in the input
-    if (userInput.trim() !== "") {
-      setShowDropdown(true);
-    } else {
-      setShowDropdown(false);
-    }
-  }, [userInput]);
-
   const handleInputChange = (event) => {
     setUserInput(event.target.value);
   };
 
-  const handleSubmit = () => {
-    handleNewCustomerEvent(userInput)
-      .then((response) => {
-        console.log(response.text);
-      })
-      .catch((err) => console.error(err));
-    setUserInput("");
+  const handleSuggestions = () => {
+    // Mocking suggestions for demonstration purposes. Call ajax API for predictive search
+    setSuggestions(["Suggestion 1", "Suggestion 2", "Suggestion 3"]);
+  };
+
+  const handleEnterPress = () => {
+    if (userInput.trim() !== "") {
+      // Update chat thread with the new message
+      setChatThread((prevChatThread) => [
+        ...prevChatThread,
+        { user: "You", message: userInput },
+      ]);
+
+      // Reset the search box text
+      setUserInput("");
+    }
   };
 
   const handleIconClick = () => {
     setShowSearchBar(!showSearchBar);
-    // Reset dropdown when search bar is toggled
-    setShowDropdown(false);
+
+    // Fetch suggestions when search bar is opened
+    if (!showSearchBar) {
+      handleSuggestions();
+    }
   };
 
   const handleDropdownItemClick = (item) => {
-    console.log("Selected item:", item);
-    setShowDropdown(false);
+    // Handle the selection of suggestions if needed
+    console.log("Selected suggestion:", item);
   };
 
   return (
     <div className="mt-4" style={{ position: "relative" }}>
       {/* Icon */}
-      <div
-        ref={iconRef}
-        onClick={handleIconClick}
-      >
+      <div ref={iconRef} onClick={handleIconClick}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           height="1em"
           viewBox="0 0 512 512"
-          style={{ fill: "white" }}  // Set icon color to white
+          style={{ fill: "white" }} // Set icon color to white
         >
           <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" />
         </svg>
@@ -90,63 +85,89 @@ export default function App({ home }) {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              flexDirection: "column",
             }}
           >
             <input
               type="text"
               value={userInput}
               onChange={handleInputChange}
-              placeholder="Search..."
+              onKeyDown={(e) => e.key === "Enter" && handleEnterPress()}
+              placeholder="Type and press Enter..."
               style={{
                 width: "80%",
                 padding: "8px",
-                fontSize: "1.5rem",
+                fontSize: "1rem",
                 borderRadius: "4px",
                 color: "black",
+                marginBottom: "8px",
               }}
             />
-            <button onClick={handleSubmit}>Search</button>
           </div>
 
-          {/* Dropdown items */}
-          {showDropdown && (
+          {/* Suggestions and Chat thread Container */}
+          <div
+            style={{
+              background: "black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "start",
+              flexDirection: "row",
+              height: "30vh",
+            }}
+          >
+            {/* Suggestions Container */}
+            <div
+              style={{
+                display: "flex",
+                width: "30%",
+                justifyContent: "flex-start",
+                flexDirection: "column",
+                color: "white",
+                overflowY: "auto", // Make it scrollable
+                maxHeight: "30vh", // Set maximum height
+              }}
+            >
+              {/* Suggestions */}
+              {suggestions.map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleDropdownItemClick(item)}
+                  style={{
+                    padding: "8px",
+                    borderBottom: "1px solid #ccc",
+                    boxSizing: "border-box",
+                    textAlign: "center",
+                    cursor: "pointer",
+                  }}
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+
+            {/* Chat thread Container */}
             <div
               style={{
                 background: "black",
                 display: "flex",
+                width: "70%",
                 flexDirection: "column",
                 alignItems: "center",
-                marginTop: "5px", // Adjust this value as needed
+                overflowY: "auto",
+                padding: "8px",
+                color: "white",
+                maxHeight: "30vh", // Set maximum height
               }}
             >
-              <div
-                onClick={() => handleDropdownItemClick("Item 1")}
-                style={{
-                  width: "80%",
-                  padding: "8px",
-                  borderBottom: "1px solid #ccc",
-                  boxSizing: "border-box",
-                  textAlign: "center",
-                  color: "white",
-                }}
-              >
-                Item 1
-              </div>
-              <div
-                onClick={() => handleDropdownItemClick("Item 2")}
-                style={{
-                  width: "80%",
-                  padding: "8px",
-                  boxSizing: "border-box",
-                  textAlign: "center",
-                  color: "white",
-                }}
-              >
-                Item 2
-              </div>
-              {/* Add more items as needed */}
+              {/* Chat thread */}
+              {chatThread.map((message, index) => (
+                <div key={index} style={{ marginBottom: "8px" }}>
+                  <strong>{message.user}:</strong> {message.message}
+                </div>
+              ))}
             </div>
-          )}
+          </div>
         </div>
       )}
     </div>
