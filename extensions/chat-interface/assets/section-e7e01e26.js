50296a926894dcb494b94825eeb901e0a1d475c1
oniken <kentaonex@gmail.com>
2023-11-30 16:19:43 -0500

    Clean up file names and structure

--- extensions/chat-interface/assets/section-e7e01e26.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,A as o}from"./App-029aa8bc.js";/* empty css                */e.createRoot(document.getElementById("section")).render(t.jsx(o,{props:{home,mountDiv:"section"}}));
