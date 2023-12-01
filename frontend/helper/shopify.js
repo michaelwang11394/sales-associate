9572c89bd8819c08841a7cfb8308fd3046ad69c7
oniken <kentaonex@gmail.com>
2023-12-01 14:06:19 -0500

    Fetch catalog with REST API

--- frontend/helper/shopify.js
+++ frontend/helper/shopify.js
@@ -1,23 +1,34 @@
-// Function to retrieve suggestions based on a search query
-export const getSuggestions = async (query) => {
+const shopifyRestQuery = async (endpoint) => {
   try {
-    return fetch(window.Shopify.routes.root + `search/suggest.json?q=${query}&resources[type]=product&resources[options][unavailable_products]=hide&resources[options][fields]=title,product_type,variants.title`)
+    return fetch(window.Shopify.routes.root + endpoint)
       .then((response) => response.json())
-      .then((suggestions) => {
-        const productSuggestions = suggestions.resources.results.products;
-
-        if (productSuggestions.length > 0) {
-          console.log(productSuggestions);
-          return productSuggestions;
-        } else {
-          return [];
-        }
+      .then((json) => {
+        return json
       });
   } catch (error) {
-    console.error('Error fetching suggestions:', error.message);
-    return [];
+    console.error(`Error fetching endpoint ${endpoint}: with message %s`, error.message);
+    return null;
   }
+}
+
+const formatCatalogEntry = (product) => {
+  // Fields we care about
+  const { title, body_html: description, id } = product;
+  return JSON.stringify({ title, description, id })
+}
+
+// Function to retrieve suggestions based on a search query
+export const getSuggestions = async (query) => {
+  const json = await shopifyRestQuery(`search/suggest.json?q=${query}&resources[type]=product&resources[options][unavailable_products]=hide&resources[options][fields]=title,product_type,variants.title`)
+  return json?.resources?.results?.products
 };
+
+export const getProducts = async () => {
+  // TODO: paginate for larger stores
+  const json = await shopifyRestQuery('products.json?limit=250&status=active')
+  console.log(json)
+  return json?.products?.map(product => formatCatalogEntry(product)).join("\r\n")
+}
 
 // Example usage
 /*
