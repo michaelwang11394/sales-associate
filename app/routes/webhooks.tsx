966548da55798e54b91cd61c1f8e15a449d7d18d
oniken <kentaonex@gmail.com>
2023-12-16 13:41:34 -0500

    Deprecate prisma

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,13 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
 
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
 export const action = async ({ request }) => {
   const { topic, shop, session } = await authenticate.webhook(request);
 
   switch (topic) {
     case "APP_UNINSTALLED":
       if (session) {
-        await db.session.deleteMany({ where: { shop } });
+        const { error } = await supabase
+          .from("merchants")
+          .delete()
+          .eq("shop", shop);
+        if (error) {
+          console.error("Shop's session deletion failed");
+        }
       }
       break;
     case "CUSTOMERS_DATA_REQUEST":
---
+++ app/supabase.session.ts
@@ -0,0 +1,65 @@
+import { Session } from "@shopify/shopify-api";
+import type { SessionStorage } from "@shopify/shopify-app-session-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
+
+export class SupabaseSessionStorage implements SessionStorage {
+  public async storeSession(session: Session): Promise<boolean> {
+    const { error } = await supabase
+      .from("merchants")
+      .upsert([
+        {
+          id: session.id,
+          shop: session.shop,
+          state: session.state,
+          isOnline: session.isOnline,
+          scope: session.scope,
+          expires: session.expires,
+          accessToken: session.accessToken,
+        },
+      ])
+      .select();
+    return !error;
+  }
+
+  public async loadSession(id: string): Promise<Session | undefined> {
+    const { data, error } = await supabase
+      .from("merchants")
+      .select("*")
+      .eq("id", id);
+
+    if (!error && data?.length === 1) {
+      return new Session(data[0]);
+    }
+
+    console.error("session load failed");
+
+    return undefined;
+  }
+
+  public async deleteSession(id: string): Promise<boolean> {
+    const { error } = await supabase.from("merchants").delete().eq("id", id);
+    return !error;
+  }
+
+  public async deleteSessions(ids: string[]): Promise<boolean> {
+    const { error } = await supabase.from("merchants").delete().in("id", ids);
+    return !error;
+  }
+
+  public async findSessionsByShop(shop: string): Promise<Session[]> {
+    const { data, error } = await supabase
+      .from("merchants")
+      .select("*")
+      .eq("shop", shop);
+    if (error || !data) {
+      console.error("No sessions found");
+      return [];
+    }
+    return data;
+  }
+}
