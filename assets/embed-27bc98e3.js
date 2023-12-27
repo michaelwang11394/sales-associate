895e48401d38b6ef4594f4dedc720569f5930ed0
oniken <kentaonex@gmail.com>
2023-12-27 09:53:01 -0800

    Remove unrelated vite generated files

--- assets/embed-27bc98e3.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as p,A as t}from"./App-029aa8bc.js";/* empty css              */e.createRoot(document.getElementById("embed-app")).render(p.jsx(t,{props:{home,mountDiv:"embed-app"}}));
--- assets/theme-1c5c6db7.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t,A as e}from"./App-029aa8bc.js";/* empty css              */o.createRoot(document.getElementById("root")).render(t.jsx(e,{props:{home,mountDiv:"root"}}));
