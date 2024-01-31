6919a866b3d0ae93a64a0315f86b5a85103e70fa
oniken <kentaonex@gmail.com>
2024-01-30 18:30:08 -0800

    Fix tailwind

--- frontend/helper/animations.ts
+++ frontend/helper/animations.ts
@@ -1,3 +1,9 @@
 export const toggleOverlayVisibility = async (overlayDiv) => {
   overlayDiv.classList.toggle("visible");
+  const visible = overlayDiv.classList.contains("visible");
+  overlayDiv.style.transition = visible
+    ? "opacity 200ms ease, visibility 0s ease 0s"
+    : "opacity 200ms ease, visibility 0s ease 200ms";
+  overlayDiv.style.opacity = visible ? 1 : 0;
+  overlayDiv.style.visibility = visible ? "visible" : "hidden";
 };
