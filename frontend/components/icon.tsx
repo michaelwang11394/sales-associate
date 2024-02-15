8147c06dff73f473a02c0522ff2dc667342187eb
oniken <kentaonex@gmail.com>
2024-02-14 16:21:16 -0800

    Add 'X' to embed message

--- frontend/components/icon.tsx
+++ frontend/components/icon.tsx
@@ -122,6 +122,7 @@ export default function Icon({ props }) {
           {/* Overlay Bubble */}
           {props.mountDiv === "embed" && iconRef.current && (
             <div
+              id="popup"
               className="talk-bubble mobile-talk-bubble"
               style={{
                 position: "absolute",
@@ -130,6 +131,53 @@ export default function Icon({ props }) {
               }}>
               <div className="talktext flex-1" style={{ whiteSpace: "normal" }}>
                 <p>{greeting}</p>
+                <div
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    cursor: "pointer",
+                    background: "#f0f0f0", // Slightly gray background
+                    borderRadius: "50%", // Added round border
+                    padding: "0px 0px 0px 4px",
+                    borderColor: "#d0d0d0", // Added border color
+                    borderWidth: "1px", // Added border width
+                    borderStyle: "solid", // Added border style
+                    width: "20px", // Ensure equal width and height
+                    height: "20px",
+                  }}
+                  onClick={(event) => {
+                    event?.stopPropagation();
+                    const popupDiv = document.getElementById("popup");
+                    if (popupDiv) {
+                      popupDiv.style.visibility =
+                        popupDiv.style.visibility === "hidden"
+                          ? "visible"
+                          : "hidden";
+                    }
+                  }} // Assuming toggleOverlayVisibility can hide the div
+                >
+                  <svg
+                    style={{
+                      marginBottom: "1px",
+                    }}
+                    fill="#000000"
+                    height="10px"
+                    width="10px"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 460.775 460.775">
+                    <path
+                      d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           )}
