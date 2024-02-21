61c61c42d97d9d8fc622b3b21aaa6dc55385702e
oniken <kentaonex@gmail.com>
2024-02-20 19:40:48 -0800

    Drive pixel events locally

--- frontend/helper/supabase.ts
+++ frontend/helper/supabase.ts
@@ -1,7 +1,6 @@
 import {
   API_URL,
   SUPABASE_EVENTS_CART_ITEMS_ENDPOINT,
-  SUPABASE_EVENTS_LAST_EVENT_ENDPOINT,
   SUPABASE_EVENTS_NEW_CUSTOMER_ENDPOINT,
   SUPABASE_EVENTS_OFFER_COUPON_ENDPOINT,
   SUPABASE_EVENTS_TABLE,
@@ -11,7 +10,7 @@ import {
   SUPABASE_MESSAGES_PRODUCTS_MENTIONED_ENDPOINT,
   SUPABASE_MESSAGES_TABLE,
   SUPABASE_PATH,
-  V1,
+  V1
 } from "@/constants/constants";
 import type { ApiResponse } from "@/constants/types";
 import { HTTPHelper } from "./http";
@@ -80,25 +79,6 @@ export const insertMessage = async (
     }
   );
   return res.body;
-};
-
-export const getLastPixelEvent = async (clientId) => {
-  try {
-    const res = await HTTPHelper.get<ApiResponse>(
-      API_URL,
-      [
-        V1,
-        SUPABASE_PATH,
-        SUPABASE_EVENTS_TABLE,
-        SUPABASE_EVENTS_LAST_EVENT_ENDPOINT,
-      ],
-      { store: store, clientId: clientId }
-    );
-    return res.body;
-  } catch (error) {
-    console.error("Error", error);
-    return { success: false, message: "An unexpected error occurred." };
-  }
 };
 
 export const isNewCustomer = async (clientId) => {
